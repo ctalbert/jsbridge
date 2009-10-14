2f2d4522c2dd99d46bec59084fe6ba32c9135055
Gordon McCreight <gordon@mccreight.com>
2009-10-15 01:59:44 +0800

    Removed the "summary" distribution option
    Setuptools was giving warnings about "summary" being an unknown option.
    The Mozmill code doesn't contain a "summary" option, favoring
    "long_description" instead, so that's what we should use here, too.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ if not sys.version.startswith('2.6'):
 setup(name=PACKAGE_NAME,
       version=PACKAGE_VERSION,
       description=desc,
-      summary=summ,
+      long_description=summ,
       author='Mikeal Rogers, Mozilla',
       author_email='mikeal.rogers@gmail.com',
       url='http://mikeal.github.com/jsbridge/',
